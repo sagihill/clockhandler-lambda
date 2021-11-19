ecf5c047769a223fd63b3226ba478ff0eb482acb
Sagi Hillel <sagi@noosa.io>
2021-11-19 16:23:45 +0200

    'deploy'

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-import { insertPrice } from "./database";
-
 const https = require("https");
 type APIResponse = { optionChain: { result: [{ quote: { bid: number } }] } };
 
@@ -34,7 +32,7 @@ exports.handler = (event, context, callback) => {
         }
         const price = response.optionChain.result[0].quote.bid;
         console.log(price);
-        // insertPrice(event.symbol, price);
+        insertPrice(event.symbol, price);
         // insertPrice(event.symbol, price).then((res) => {
         //   console.log(res);
         // });
@@ -48,4 +46,19 @@ exports.handler = (event, context, callback) => {
   }
 };
 
-function lambdafunc(event, callback) {}
+// import { knex as Knex } from "knex";
+
+// const connection = {
+//   ssl: { rejectUnauthorized: false },
+//   host: "tradewatch-db.cluster-ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
+//   user: "admin",
+//   password: "sagi1991",
+//   database: "tradewatch-db",
+// };
+
+// const knex = Knex({ client: "mysql", connection });
+
+export function insertPrice(symbol: string, price: number): void {
+  //   const res = await knex("Prices").insert({ symbol, price });
+  console.log(symbol);
+}
