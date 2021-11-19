8820b77b835a9ab2f9ca96eeb4a0d444bbd85adc
Sagi Hillel <sagi@noosa.io>
2021-11-19 16:36:12 +0200

    'deploy'

--- index.ts
+++ index.ts
@@ -49,16 +49,15 @@ exports.handler = (event, context, callback) => {
 // import { knex as Knex } from "knex";
 
 const knex = require("knex")
+const connection = {
+  ssl: { rejectUnauthorized: false },
+  host: "tradewatch-db.cluster-ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
+  user: "admin",
+  password: "sagi1991",
+  database: "tradewatch-db",
+};
 
-// const connection = {
-//   ssl: { rejectUnauthorized: false },
-//   host: "tradewatch-db.cluster-ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
-//   user: "admin",
-//   password: "sagi1991",
-//   database: "tradewatch-db",
-// };
-
-// const knex = Knex({ client: "mysql", connection });
+const knexjs = knex({ client: "mysql", connection });
 
 function insertPrice(symbol: string, price: number): void {
   //   const res = await knex("Prices").insert({ symbol, price });
