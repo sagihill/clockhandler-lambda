5e75329bbd9721f2c0ac356fa48fe1ce49eb04d9
Sagi Hillel <sagi@noosa.io>
2021-11-19 15:34:34 +0200

    'deploy'

--- database.ts
+++ database.ts
@@ -1,5 +1,4 @@
-import { knex as Knex } from "knex";
-
+const knex = require("knex");
 const connection = {
   ssl: { rejectUnauthorized: false },
   host: "tradewatch-db.cluster-ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
@@ -8,12 +7,12 @@ const connection = {
   database: "tradewatch-db",
 };
 
-const knex = Knex({ client: "mysql", connection });
+const knexjs = knex({ client: "pg", connection });
 
 export async function insertPrice(
   symbol: string,
   price: number
 ): Promise<void> {
-//   const res = await knex("Prices").insert({ symbol, price });
+  //   const res = await knex("Prices").insert({ symbol, price });
   console.log("HI");
 }
--- index.ts
+++ index.ts
@@ -11,34 +11,38 @@ type APIResponse = { optionChain: { result: [{ quote: { bid: number } }] } };
  * Will succeed with the response body.
  */
 exports.handler = (event, context, callback) => {
-  const options = {
-    method: "get",
-    hostname: "yfapi.net",
-    path: "https://yfapi.net/v7/finance/options/" + event.symbol,
-    headers: {
-      "x-api-key": "L5KARBpGqm6aQPrXPfwgl6E5Ild5pRBh8dG7cb6a",
-    },
-  };
+  try {
+    const options = {
+      method: "get",
+      hostname: "yfapi.net",
+      path: "https://yfapi.net/v7/finance/options/" + event.symbol,
+      headers: {
+        "x-api-key": "L5KARBpGqm6aQPrXPfwgl6E5Ild5pRBh8dG7cb6a",
+      },
+    };
 
-  const req = https.request(options, (res) => {
-    let body = "";
-    let response: APIResponse = null;
-    res.on("data", (chunk) => (body += chunk));
-    res.on("end", () => {
-      console.log("Successfully processed HTTPS response");
-      // If we know it's JSON, parse it
-      if (res.headers["content-type"] === "application/json") {
-        body = JSON.parse(body);
-        response = body as unknown as APIResponse;
-      }
-      const price = response.optionChain.result[0].quote.bid;
-      console.log(price);
-      insertPrice(event.symbol, price).then((res) => {
-        console.log(res);
-        callback(null, true);
+    const req = https.request(options, (res) => {
+      let body = "";
+      let response: APIResponse = null;
+      res.on("data", (chunk) => (body += chunk));
+      res.on("end", () => {
+        console.log("Successfully processed HTTPS response");
+        // If we know it's JSON, parse it
+        if (res.headers["content-type"] === "application/json") {
+          body = JSON.parse(body);
+          response = body as unknown as APIResponse;
+        }
+        const price = response.optionChain.result[0].quote.bid;
+        console.log(price);
+        insertPrice(event.symbol, price).then((res) => {
+          console.log(res);
+          callback(null, true);
+        });
       });
     });
-  });
-  req.on("error", callback);
-  req.end();
+    req.on("error", callback);
+    req.end();
+  } catch (error) {
+    console.log(error);
+  }
 };
