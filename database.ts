8b99ce0398bb674a0269fd882b4118cbee1c7733
Sagi Hillel <sagi@noosa.io>
2021-11-19 15:16:06 +0200

    'deploy'

--- database.ts
+++ database.ts
@@ -14,6 +14,6 @@ export async function insertPrice(
   symbol: string,
   price: number
 ): Promise<void> {
-  const res = await knex("Prices").insert({ symbol, price });
-  console.log(res);
+//   const res = await knex("Prices").insert({ symbol, price });
+  console.log("HI");
 }
