9377248cd9e7dd375b8ac3853b9adc5b3c4b05d2
Sagi Hillel <sagi@noosa.io>
2021-11-19 14:56:18 +0200

    try now

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import { insertPrice } from "./database";
 
 const https = require("https");
 type APIResponse = { optionChain: { result: [{ quote: { bid: number } }] } };
@@ -40,3 +39,27 @@ exports.handler = async (event: { symbol: string }, context, callback) => {
   req.on("error", callback);
   req.end();
 };
+
+
+import * as AWS from "aws-sdk";
+import { Context } from "aws-lambda";
+import { knex as Knex } from "knex";
+AWS.config.update({ region: "eu-central-1" });
+
+const connection = {
+  ssl: { rejectUnauthorized: false },
+  host: "tradewatch-db.cluster-ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
+  user: "admin",
+  password: "sagi1991",
+  database: "tradewatch-db",
+};
+
+const knex = Knex({ client: "mysql", connection });
+
+export async function insertPrice(
+  symbol: string,
+  price: number
+): Promise<void> {
+  const res = await knex("Prices").insert({ symbol, price });
+  console.log(res);
+}
