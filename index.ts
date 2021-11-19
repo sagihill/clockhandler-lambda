7afd1d7f34bf05fbc0f4bb4f86d0c5d40bbb346d
Sagi Hillel <sagi@noosa.io>
2021-11-19 23:35:47 +0200

    'deploy'

--- index.ts
+++ index.ts
@@ -51,8 +51,8 @@ exports.handler = (event, context, callback) => {
 const connection = {
   ssl: { rejectUnauthorized: false },
   host: "tradewatch-db.cluster-ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
-  user: "admin",
-  password: "sagi1991",
+  user: "RDSUser",
+  password: "S1A2gi34",
   database: "tradewatch-db",
 };
 const knex = require("knex")({
