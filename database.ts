905ec88c4303ae7edcfc9838357265ea1983f48b
Sagi Hillel <sagi@noosa.io>
2021-11-19 15:03:24 +0200

    'deploy'

---
+++ database.ts
@@ -0,0 +1,19 @@
+import { knex as Knex } from "knex";
+
+const connection = {
+  ssl: { rejectUnauthorized: false },
+  host: "tradewatch-db.cluster-ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
+  user: "admin",
+  password: "sagi1991",
+  database: "tradewatch-db",
+};
+
+const knex = Knex({ client: "mysql", connection });
+
+export async function insertPrice(
+  symbol: string,
+  price: number
+): Promise<void> {
+  const res = await knex("Prices").insert({ symbol, price });
+  console.log(res);
+}
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { insertPrice } from "./database";
 
 const https = require("https");
 type APIResponse = { optionChain: { result: [{ quote: { bid: number } }] } };
@@ -32,34 +33,10 @@ exports.handler = async (event: { symbol: string }, context, callback) => {
       }
       const price = response.optionChain.result[0].quote.bid;
       console.log(price)
-      // await insertPrice(event.symbol, price);
+      await insertPrice(event.symbol, price);
     });
   });
   callback(null, true);
   req.on("error", callback);
   req.end();
 };
-
-
-// import * as AWS from "aws-sdk";
-// import { Context } from "aws-lambda";
-// import { knex as Knex } from "knex";
-// AWS.config.update({ region: "eu-central-1" });
-
-// const connection = {
-//   ssl: { rejectUnauthorized: false },
-//   host: "tradewatch-db.cluster-ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
-//   user: "admin",
-//   password: "sagi1991",
-//   database: "tradewatch-db",
-// };
-
-// const knex = Knex({ client: "mysql", connection });
-
-// export async function insertPrice(
-//   symbol: string,
-//   price: number
-// ): Promise<void> {
-//   const res = await knex("Prices").insert({ symbol, price });
-//   console.log(res);
-// }
