70b9cc19fe8bafe4336a1611b008ae16d9a8bcf3
Sagi Hillel <sagi@noosa.io>
2021-11-20 13:41:50 +0200

    'deploy'

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
-const https = require("https");
 const axios = require("axios").default;
 type APIResponse = {
   optionChain: { result: [{ quote: { regularMarketPrice: number } }] };
 };
 
-exports.handler = async (event: { symbol: string }) => {
+type ClockHandlerEvent = {
+  symbol: string;
+};
+
+exports.handler = async (event: ClockHandlerEvent) => {
   try {
     const price = await getPrice(event.symbol);
-    // const priceId = await insertPrice(event.symbol, price);
-    const priceId = await insertPrices(event.symbol, price);
+    const priceId = await insertPrice(event.symbol, price);
     return priceId;
   } catch (error) {
     console.log(error);
@@ -34,20 +36,20 @@ async function getPrice(symbol: string): Promise<number> {
   return price;
 }
 
-const connection = {
-  ssl: { rejectUnauthorized: false },
-  host: "tradewatch-1.ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
-  port: "3306",
-  user: "admin",
-  password: "sagi1991",
-  database: "TradeWatch",
-};
-const knex = require("knex")({
-  client: require("knex/lib/dialects/mysql"),
-  connection,
-});
-
 async function insertPrice(symbol: string, price: number): Promise<number> {
+  const connection = {
+    ssl: { rejectUnauthorized: false },
+    host: "tradewatch-1.ckjhl9zn95xm.eu-central-1.rds.amazonaws.com",
+    port: "3306",
+    user: "admin",
+    password: "sagi1991",
+    database: "TradeWatch",
+  };
+  const knex = require("knex")({
+    client: require("knex/lib/dialects/mysql"),
+    connection,
+  });
+
   const res = await knex("Prices").insert({ symbol, price });
   return res[0];
 }
